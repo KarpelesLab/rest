54645d66bc43ade5ebf95e6fb364421639a8781b
Mark Karpeles <magicaltux@gmail.com>
2020-10-19 19:47:14 +0900

    adding router.go

---
+++ router.go
@@ -0,0 +1,14 @@
+package rest
+
+import "net/http"
+
+type RouterType struct {
+}
+
+var Router *RouterType = &RouterType{}
+
+func (h *RouterType) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// fallback to PHP, add prefix for rest
+	req.URL.Path = "/_special/rest" + req.URL.Path
+	SystemProxy.ServeHTTP(w, req)
+}
