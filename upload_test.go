b3cb4fa9458445efd445dfd34df23eacfe43015d
Mark Karpeles <magicaltux@gmail.com>
2021-06-12 17:11:12 +0900

    add expected hash to test in order to confirm parallel upload does not cause corruption

--- upload_test.go
+++ upload_test.go
@@ -3,6 +3,8 @@ package rest
 import (
 	"context"
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
 	"io"
 	"log"
 	"testing"
@@ -10,7 +12,11 @@ import (
 
 func TestUpload(t *testing.T) {
 	// input file (512MB, non seekable)
-	input := &io.LimitedReader{R: rand.Reader, N: 512 * 1024 * 1024}
+	var input io.Reader
+	input = &io.LimitedReader{R: rand.Reader, N: 512 * 1024 * 1024}
+	// compute sha256 of bytes we send
+	hash := sha256.New()
+	input = io.TeeReader(input, hash)
 
 	ctx := context.Background()
 	res, err := Upload(ctx, "Shell/Bit:upload", "POST", Param{"filename": "test.bin"}, input, "application/octet-stream")
@@ -19,5 +25,6 @@ func TestUpload(t *testing.T) {
 		t.Fatalf("failed to do upload: %s", err)
 	}
 
+	log.Printf("expected hash = %s", hex.EncodeToString(hash.Sum(nil)))
 	log.Printf("res = %s", res.Data)
 }
