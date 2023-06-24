8c69948c345ce100d365d817ef6b7fb3981a859c
Mark Karpeles <magicaltux@gmail.com>
2023-06-25 07:15:17 +0900

    add sender directly into rest

---
+++ sender.go
@@ -0,0 +1,22 @@
+package rest
+
+import (
+	"context"
+	"io"
+)
+
+type SenderInterface interface {
+	Send(from string, to []string, msg io.WriterTo) error
+}
+
+type restSender struct{}
+
+var Sender SenderInterface = restSender{}
+
+func (s restSender) Send(from string, to []string, msg io.WriterTo) error {
+	reader, writer := io.Pipe()
+	defer writer.Close()
+	go msg.WriteTo(writer)
+	_, err := Upload(context.Background(), "MTA:send", "POST", map[string]any{"from": from, "to": to}, reader, "message/rfc822")
+	return err
+}
