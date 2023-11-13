3730801c4426ea2af6dc7e0486f7797e0e6b287a
Mark Karpeles <magicaltux@gmail.com>
2023-11-13 22:11:18 +0900

    tweak parameters for lower quality lines

--- client.go
+++ client.go
@@ -8,9 +8,12 @@ import (
 var RestHttpTransport = &http.Transport{
 	Proxy:                 http.ProxyFromEnvironment,
 	MaxIdleConns:          100,
+	MaxIdleConnsPerHost:   50,
+	MaxConnsPerHost:       200,
 	IdleConnTimeout:       90 * time.Second,
+	ResponseHeaderTimeout: 90 * time.Second,
 	TLSHandshakeTimeout:   10 * time.Second,
-	ExpectContinueTimeout: 1 * time.Second,
+	ExpectContinueTimeout: 5 * time.Second,
 }
 
 var RestHttpClient = &http.Client{
