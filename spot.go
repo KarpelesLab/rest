17b7b0f81acd902c88c2982c8f5bfe8f7e53cab1
Mark Karpeles <magicaltux@gmail.com>
2024-09-25 09:10:24 +0900

    add spot

---
+++ spot.go
@@ -0,0 +1,50 @@
+package rest
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/KarpelesLab/pjson"
+)
+
+// SpotClient is an interface fullfilled by spotlib.Client that contains
+// everything we really care about, and helps avoid dependency loops
+type SpotClient interface {
+	Query(ctx context.Context, target string, body []byte) ([]byte, error)
+}
+
+func SpotApply(ctx context.Context, client SpotClient, path, method string, param any, target any) error {
+	res, err := SpotDo(ctx, client, path, method, param)
+	if err != nil {
+		return err
+	}
+	err = pjson.UnmarshalContext(ctx, res.Data, target)
+	if Debug && err != nil {
+		slog.ErrorContext(ctx, fmt.Sprintf("failed to parse json: %s\n%s", err, res.Data), "event", "rest:not_json")
+	}
+	return err
+}
+
+func SpotDo(ctx context.Context, client SpotClient, path, method string, param any) (*Response, error) {
+	req := map[string]any{
+		"path":   path,
+		"verb":   method,
+		"params": param,
+	}
+	buf, err := pjson.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+	respbuf, err := client.Query(ctx, "@/p_api", buf)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp *Response
+	err = pjson.Unmarshal(respbuf, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
