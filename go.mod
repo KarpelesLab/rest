5cb6ed7a41f06ac5fb702051ce50eedde176308c
Mark Karpeles <magicaltux@gmail.com>
2025-02-16 09:37:50 +0900

    add token sender for token holders

--- go.mod
+++ go.mod
@@ -6,6 +6,6 @@ toolchain go1.21.3
 
 require (
 	github.com/KarpelesLab/pjson v0.1.9
-	github.com/KarpelesLab/typutil v0.2.17
+	github.com/KarpelesLab/typutil v0.2.29
 	github.com/KarpelesLab/webutil v0.2.2
 )
--- sender.go
+++ sender.go
@@ -9,6 +9,10 @@ type SenderInterface interface {
 	Send(from string, to []string, msg io.WriterTo) error
 }
 
+type TokenSender struct {
+	Token string
+}
+
 type restSender struct{}
 
 var Sender SenderInterface = restSender{}
@@ -21,5 +25,16 @@ func (s restSender) Send(from string, to []string, msg io.WriterTo) error {
 		msg.WriteTo(writer)
 	}()
 	_, err := Upload(context.Background(), "MTA:send", "POST", map[string]any{"from": from, "to": to}, reader, "message/rfc822")
+	return err
+}
+
+func (s *TokenSender) Send(from string, to []string, msg io.WriterTo) error {
+	reader, writer := io.Pipe()
+	defer reader.Close()
+	go func() {
+		defer writer.Close()
+		msg.WriteTo(writer)
+	}()
+	_, err := Upload(context.Background(), "MTA:send", "POST", map[string]any{"from": from, "to": to, "token": s.Token}, reader, "message/rfc822")
 	return err
 }
